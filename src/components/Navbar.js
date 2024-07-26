89c38eceb136ed1e1052adfaccdfea0048376fe6
Raj Kumar <119596365+kumar-modukuri@users.noreply.github.com>
2024-07-26 09:10:55 +0000

    search auto

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({searched}) => {
     const [inputValue,setInputValue] = useState("");
 
-    const handleClick = () => {
+    useEffect(() => {
         searched(inputValue);
-    }
+    },[inputValue,searched]);
 
     return ( 
         <header>
@@ -14,14 +14,11 @@ const Navbar = ({searched}) => {
                 <div>
                     <Link to="/">
                         <img src={require('../assets/rk_logo.jpg')} alt='Logo' className='logo'/>
-                        <h2 className='titleMain'>PROJECT 01</h2>
+                        <h2 className='titleMain'>MY PROJECT</h2>
                     </Link>
                 </div>
                 <div className='search-about-container'>
-                    <div className='search-wrapper'>
-                        <input className='search-bar' placeholder='Search...' onChange={(e) => setInputValue(e.target.value)} value={inputValue}/>
-                        <img src={require('../assets/Search-Logo.png')} alt='Button Logo' className='search-btn' onClick={handleClick}/>
-                    </div>
+                    <input className='search-bar' placeholder='Search...' onChange={(e) => setInputValue(e.target.value)} value={inputValue}/>
                     <Link to="/about">
                         <p className='about'>About</p>
                     </Link>
