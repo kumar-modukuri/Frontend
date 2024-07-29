454de3c8d165c5858d6ac1ed550af558368c6fbe
Raj Kumar <119596365+kumar-modukuri@users.noreply.github.com>
2024-07-29 02:44:34 +0000

    search fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,34 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const Navbar = ({searched}) => {
-    const [inputValue,setInputValue] = useState("");
+const Navbar = ({ searched }) => {
+    const [inputValue, setInputValue] = useState("");
 
     useEffect(() => {
         searched(inputValue);
-    },[inputValue,searched]);
+    }, [inputValue, searched]);
 
-    return ( 
+    const location = useLocation();
+    const isHomePage = location.pathname === '/';
+
+    return (
         <header>
             <div className="container">
                 <div>
                     <Link to="/">
-                        <img src={require('../assets/rk_logo.jpg')} alt='Logo' className='logo'/>
+                        <img src={require('../assets/rk_logo.jpg')} alt='Logo' className='logo' />
                         <h2 className='titleMain'>MY PROJECT</h2>
                     </Link>
                 </div>
                 <div className='search-about-container'>
-                    <input className='search-bar' placeholder='Search...' onChange={(e) => setInputValue(e.target.value)} value={inputValue}/>
+                    {isHomePage && (<input className='search-bar' placeholder='Search...' onChange={(e) => setInputValue(e.target.value)} value={inputValue}/>)}
                     <Link to="/about">
                         <p className='about'>About</p>
                     </Link>
                 </div>
             </div>
         </header>
-     );
+    );
 }
 
 export default Navbar;
